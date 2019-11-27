e8ec89cbf3a6bf42aefbea30ff718b8f1a02345b
Hugothms <hugothms@gmail.com>
2019-11-27 18:38:23 +0100

    variadic printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,15 +6,23 @@
 /*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/27 13:42:33 by hthomas           #+#    #+#             */
-/*   Updated: 2019/11/27 18:21:10 by hthomas          ###   ########.fr       */
+/*   Updated: 2019/11/27 18:36:31 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"ft_printf.h"
 
-int	ft_printf(const char *str, ...)
+int	ft_printf(const char *format, ...)
 {
-	return (0);
+	va_list	args;
+	int		done;
+
+	va_start(args, format);
+	done = vfprintf(stdout, format, args);
+	// TODO write code here
+
+	va_end(args);
+	return (done);
 }
 
 
